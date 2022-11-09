54d31b97642faa3b4384557e212df2338098386f
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2022-11-09 15:27:33 +0000

    strip whitespace in label foreground species script input

--- bin/label_foreground_species.sh
+++ bin/label_foreground_species.sh
@@ -1,3 +1,4 @@
-while read spp; do
-    sed -i '.bak' "s/$spp:/$spp{Foreground}:/" $1 
-done < $2
+grep "\S" $2 | \
+    while read spp; do
+        sed -i '.bak' "s/$spp:/$spp{Foreground}:/" $1 
+    done 
