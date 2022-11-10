fc1320007d26bd991ef2875247c9123d19ed352d
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2022-11-10 10:08:30 +0000

    Add directives and comments to label species script

--- bin/label_species.sh
+++ bin/label_species.sh
@@ -1,3 +1,7 @@
+#! /usr/bin/env bash
+
+# $1: newick format species tree
+# $2: two column format species and their labels
 grep "\S" $2 | \
     while read spp lbl; do
         sed -i '.bak' "s/$spp:/$spp{$lbl}:/" $1 
